12a9999eb088937bc802cb1a7e97a8278c6909ee
parker mccurley <parkermccurlz@gmail.com>
2016-11-16 15:54:53 -0500

    Solve Word Count in JS

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,33 @@
+// super ugly but I'm tired
+var Words = function() {};
+
+Words.prototype.count = function(input) {
+  var wordArray = normalize(input.replace(/[\n\t]/g, ' ').trim()).split(' ');
+  var result = {};
+
+  toLower(wordArray).forEach(function(item) {
+    if (result[item] && Number.isInteger(result[item]))
+      result[item] += 1;
+    else
+      result[item] = 1;
+  });
+
+  function toLower(array) {
+    console.log(array);
+    for (var i = 0; i < array.length; i++) {
+      array[i] = array[i].toLowerCase();
+    }
+    console.log(array);
+    return array;
+  }
+
+  function normalize(string) {
+    console.log(string);
+    console.log(string.replace(/\s+/g, ' '));
+    return string.replace(/\s+/g, ' ');
+  }
+
+  return result;
+}
+
+module.exports = Words;
