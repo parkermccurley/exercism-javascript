73c7b5ac7fcbf6840997b360493a171b13ae2e6a
parker mccurley <parkermccurlz@gmail.com>
2016-11-16 11:49:03 -0500

    Solve Bob for JS

---
+++ javascript/bob/bob.js
@@ -0,0 +1,53 @@
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+  var bobsay;
+
+  if (multipleSentences(input)) {
+    if (lastLetter(input) == '!')
+      bobsay = 'Whoa, chill out!';
+    else if (lastLetter(input) == '?')
+      bobsay = 'Sure.';
+    else if (lastLetter(input) == '.')
+      bobsay = 'Whatever.';
+  }
+  else if (isSilent(input))
+    bobsay = 'Fine. Be that way!';
+  else if (countUppercase(input) >= 2 && (lastLetter(input) == '!' || lastLetter(input) == '?') || isAllCaps(input))
+    bobsay = 'Whoa, chill out!';
+  else if (lastLetter(input) == '?')
+    bobsay = 'Sure.';
+  else
+    bobsay = 'Whatever.';
+
+  function countUppercase(string) {
+    return string.replace(/[^A-Z]/g, '').length;
+  }
+
+  function lastLetter(string) {
+    return string.substr(-1);
+  }
+
+  function isAllCaps(string) {
+    if (string.length == string.replace(/[^A-Z ]/g, '').length)
+      return true;
+    return false;
+  }
+
+  function multipleSentences(string) {
+    console.log(string.replace(/[^!?.]/g, ''));
+    if (string.replace(/[^!?.]/g, '').length > 1)
+      return true;
+    return false;
+  }
+
+  function isSilent(string) {
+    if (!string || (string.length == string.replace(/[^ ]/g, '').length))
+      return true;
+    return false;
+  }
+
+  return bobsay;
+};
+
+module.exports = Bob;
