ee2fc6ac38912f7eea517b947ff98eed89538bc0
parker mccurley <parkermccurlz@gmail.com>
2016-11-28 23:20:22 -0500

    Solve Grains in JS

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -1,3 +1,22 @@
+var BigInt = require('./big-integer');
+
 var Grains = function Grains() {};
 
 Grains.prototype.square = function square(which) {
+  var bigTwo = BigInt(2);
+  var power = which - 1;
+  bigTwo = bigTwo.pow(power);
+
+  return bigTwo.toString();
+};
+
+Grains.prototype.total = function total() {
+  var bigTotal = BigInt(0);
+  for (var i = 1; i <= 64; i++) {
+    bigTotal = bigTotal.add(this.square(i));
+  }
+
+  return bigTotal.toString();
+};
+
+module.exports = Grains;
